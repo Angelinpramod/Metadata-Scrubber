725beea541e883943c5ee02cfddc6b5a6bb646d0
Angelin Pramod <angelinpramod06@gmail.com>
2026-03-20 00:16:39 +0530

    just another

--- scrubber.py
+++ scrubber.py
@@ -1,3 +1,7 @@
 from PIL import Image
 image=Image.open('Kodak_CX7530.jpg')
-print(image.getexif())
+exif_data=image.getexif()
+
+for tag_id,value in exif_data.item():
+  tag_name = ExifTags.TAGS.get(tag_id, tag_id) 
+  print(f"{tag_name:25} : {value}")
