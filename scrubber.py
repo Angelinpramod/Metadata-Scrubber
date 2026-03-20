e32be3739282753bc5759c93013cdc8e43dd88b2
Angelin Pramod <angelinpramod06@gmail.com>
2026-03-20 23:30:57 +0530

    Enhance EXIF data extraction with GPS information
    Added GPS data extraction from EXIF metadata.

--- scrubber.py
+++ scrubber.py
@@ -1,7 +1,12 @@
-from PIL import Image
+from PIL import Image,ExifTags
 image=Image.open('Kodak_CX7530.jpg')
 exif_data=image.getexif()
+gps_data = exif_data.get_ifd(34853)
+print(gps_data)
 
-for tag_id,value in exif_data.item():
+for tag_id,value in exif_data.items():
   tag_name = ExifTags.TAGS.get(tag_id, tag_id) 
+  gps=ExifTags.GPSTAGS.get(tag_id,tag_id)
   print(f"{tag_name:25} : {value}")
+
+print(dir(exif_data))
